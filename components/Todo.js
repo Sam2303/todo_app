dc6a928a2c0a3131b0c28a6cf49525d570ca83dc
Sam2303 <sjp2000@live.com>
2021-08-19 09:07:18 +0100

    styling of todo table, making table DOM correct

--- components/Todo.js
+++ components/Todo.js
@@ -4,24 +4,38 @@ import { TodosContext } from "../context/TodosContext";
 const Todo = (todo) => {
   todo = todo.todo;
 
+
   const {updateTodo, deleteTodo} = useContext(TodosContext);
-  const completeBtn = () => {
+  const updateStatusBtn = (status) => {
     console.log(todo);
     const updatedFields = {
       Name: todo.fields.Name,
       Difficulty: todo.fields.Difficulty,
-      Status: "Done"
+      Status: status
     };
     console.log(updatedFields);
     const updatedTodo = { id: todo.id, fields: updatedFields };
     updateTodo(updatedTodo);
   };
+
+const deleteBtn = (id) => {
+  deleteTodo(id);
+}
+const buttonText = (status) => {
+  if (status === "Todo" || status === "In progress"){return "Update"}
+  else {return "Delete"} 
+}
+
   return (
     <tr key={todo.id}>
       <td>{todo.fields.Name}</td>
       <td>{todo.fields.Difficulty}</td>
       <td>{todo.fields.Status}</td>
-      <button onClick={completeBtn}>Complete</button>
+      <td><button onClick={() => {
+        if(todo.fields.Status === "Todo"){updateStatusBtn("In progress")}
+        else if(todo.fields.Status === "In progress"){updateStatusBtn("Done")}
+        else{deleteBtn(todo.id)}
+      }}>{buttonText(todo.fields.Status)}</button></td>
     </tr>
   );
 };
