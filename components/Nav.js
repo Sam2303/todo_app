d5f91c05f0fed423de79553b2c37d71004d5ab36
Sam2303 <sjp2000@live.com>
2021-08-25 16:36:39 +0100

    update so nav bar is correct

--- components/Nav.js
+++ components/Nav.js
@@ -6,9 +6,9 @@ const Nav = () => {
     <div className="navBar">
       <h1>Work on These</h1>
       <div className="navLinks">
-        <Link href="/">My Diary</Link>
+        {/* <Link href="/">My Diary</Link>
         <Link href="/">My Routine</Link>
-        <Link href="/">Create Routine</Link>
+        <Link href="/">Create Routine</Link> */}
       </div>
       {/* <a href="/">Login</a> */}
     </div>
